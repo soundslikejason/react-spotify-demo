be5fa98fb418a502d9ca1ddb2292739fe4bc95ab
Jason Leong <soundslikejason@users.noreply.github.com>
2017-02-14 13:57:23 -0800

    search bar with spotify

---
+++ src/components/app.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar/SearchBar';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialMessage: 'greeting',
+      song: '',
+    };
+  }
+
+  fetchSongs = () => {
+    console.log("hey i'm being clicked");
+  }
+
+  render() {
+    const { initialMessage } = this.state;
+    return (
+      <div>
+        <SearchBar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
+        {initialMessage}
+      </div>
+    );
+  }
+}
