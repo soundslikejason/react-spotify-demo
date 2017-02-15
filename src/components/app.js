531737f47eac4cf32e4dd132bb87767a3aaf8537
Jason Leong <soundslikejason@users.noreply.github.com>
2017-02-15 12:20:18 -0800

    load and render data from spotify

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import styles from './app.css';
 import SearchBar from './SearchBar/SearchBar';
 import searchSpotify from '../utils/searchSpotify';
 import SongItem from './SongItem/SongItem';
+import SongList from './SongList/Songlist';
 
 export default class App extends Component {
 
@@ -12,6 +13,7 @@ export default class App extends Component {
       initialMessage: 'greeting',
       song: '',
       tracks: {},
+      songPosition: 0,
     };
   }
 
@@ -21,12 +23,13 @@ export default class App extends Component {
   }
 
   render() {
-    const { tracks } = this.state;
-    console.log('tracks', tracks);
+    const { tracks, songPosition } = this.state;
+
     return (
       <div className={styles.root}>
         <SearchBar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
-        {tracks.items && <SongItem songData={tracks.items[0]} />}
+        {tracks.items && <SongItem songData={tracks.items[songPosition]} />}
+        {tracks.items && <SongList listOfSongs={tracks.items} selectSong={(songPosition) => this.setState({ songPosition })} />}
       </div>
     );
   }
