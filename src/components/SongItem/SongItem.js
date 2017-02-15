464626046073ce7dd64510de8f6d954b5b5b5552
Jason Leong <soundslikejason@users.noreply.github.com>
2017-02-14 19:20:43 -0800

    safeguard components

---
+++ src/components/SongItem/SongItem.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+import styles from './SongItem.css';
+
+const SongItem = (props) => {
+  console.log(props);
+  return (
+    <div>
+      SongItem
+    </div>
+  );
+};
+
+export default SongItem;
--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import searchSpotify from '../utils/searchSpotify';
+import SongItem from './SongItem/SongItem';
 
 export default class App extends Component {
 
@@ -9,20 +10,22 @@ export default class App extends Component {
     this.state = {
       initialMessage: 'greeting',
       song: '',
+      tracks: {},
     };
   }
 
   fetchSongs = () => {
     searchSpotify(this.state.song)
-      .then((tracks) => console.log('tracks', tracks));
+      .then(({ tracks }) => this.setState({ tracks }));
   }
 
   render() {
-    const { initialMessage } = this.state;
+    const { tracks } = this.state;
+    console.log('tracks', tracks);
     return (
       <div>
         <SearchBar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
-        {initialMessage}
+        {tracks.items && <SongItem songData={tracks.items[0]} />}
       </div>
     );
   }
