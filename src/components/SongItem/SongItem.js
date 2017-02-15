592c0b409ddecdd4da990f1ed494734daab7ea4c
Jason Leong <soundslikejason@users.noreply.github.com>
2017-02-15 11:06:37 -0800

    create song item component with retrieved data

--- src/components/SongItem/SongItem.js
+++ src/components/SongItem/SongItem.js
@@ -2,12 +2,28 @@ import React, { PropTypes } from 'react';
 import styles from './SongItem.css';
 
 const SongItem = (props) => {
-  console.log(props);
+  const { songData } = props;
+  const album = songData.album;
+  const albumName = album.name;
+  const albumImage = album.images[1];
+
   return (
-    <div>
-      SongItem
+    <div className={styles.root}>
+      <div className={styles.album}>
+        <img role="presentation" src={albumImage.url} />
+        <span className={styles.albumName}>{albumName}</span>
+      </div>
+      <div className={styles.songAndDescription}>
+        <audio controls duration src={songData.preview_url} />
+        <span className={styles.songDescription}>Name: {songData.name}</span>
+        <span className={styles.songDescription}>Duration: {songData.duration_ms / 1000} s</span>
+      </div>
     </div>
   );
+};
+
+SongItem.propTypes = {
+  songData: PropTypes.object,
 };
 
 export default SongItem;
